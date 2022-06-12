53dcf93dd279458d1dd69783ebaa1b6b638fdcf0
Khanh Nguyen <khanhnd185@gmail.com>
2022-06-12 22:51:20 +0700

    Update learning curve

--- plot/learn_curve.py
+++ plot/learn_curve.py
@@ -2,8 +2,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import argparse
 
-arc = 'swinbase'
+parser = argparse.ArgumentParser(description='Plot learning curve')
+parser.add_argument('--arc', '-a', type=str, help='choose architecture')
+parser.add_argument('--factor', default='all', type=str, help="choose factor to plot")
+args = parser.parse_args()
+
+arc = args.arc
+print(args.factor)
 df1 = pd.read_csv(arc + '_1.csv')
 df2 = pd.read_csv(arc + '_2.csv')
 i = np.arange(20)
@@ -19,6 +26,11 @@ df = pd.concat(
 
 print(df)  
 for k in df.keys()[1:]:
-    plt.plot(i, df[k], label=k)
+    if (args.factor not in k) and (args.factor != 'all'):
+        print('skip')
+        continue
+    else:
+        plt.plot(i, df[k], label=k)
+
 plt.legend()
 plt.show()
